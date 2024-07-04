1be8d453fb374ccf21fce26c548083973ff6e2e3
juliocesarcabrera0788 <juliocesarcabrera0788@gmail.com>
2024-07-04 23:44:40 +0200

    Inicio del proyecto, se crea el mapa y el acceso a la ubicacion

---
+++ UberClone.xcodeproj/UberClone/App/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Julio Cabrera on 2024-07-04.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Julio Cabrera on 2024-07-04.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresetable()
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    HomeView()
+}
