bfd98c2e809888a64ea3f51cac943ecae2c46201
juliocesarcabrera0788 <juliocesarcabrera0788@gmail.com>
2024-07-05 16:58:54 +0200

    Se agrega marker al punto seleccionado

--- UberClone/Core/Home/Views/UberMapViewRepresetable.swift
+++ UberClone/Core/Home/Views/UberMapViewRepresetable.swift
@@ -27,7 +27,7 @@ struct UberMapViewRepresetable: UIViewRepresentable {
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationViewModel.selectedLocationCoordinate{
-            
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
         }
     }
     
@@ -39,18 +39,42 @@ struct UberMapViewRepresetable: UIViewRepresentable {
 
 
 extension UberMapViewRepresetable {
+    
     class MapCoordinator: NSObject, MKMapViewDelegate {
+    
+        //MARK: Properties
+        
         let parent: UberMapViewRepresetable
+        
+        //MARK: Lifecycle
         
         init(parent: UberMapViewRepresetable) {
             self.parent = parent
             super.init()
         }
         
+        //MARK: MKMapViewDelegate
+        
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
             
             parent.mapView.setRegion(region, animated: true)
+        }
+        
+        
+        //MARK: Helpers
+        
+        func addAndSelectAnnotation(withCoordinate coordinate:CLLocationCoordinate2D){
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+            
         }
     }
 }
